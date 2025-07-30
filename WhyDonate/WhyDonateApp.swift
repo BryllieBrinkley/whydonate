1bfdc0eb74bd512789b08b29880d927ae34c92da
Jibryll Brinkley <jibryllbrink@yahoo.com>
2025-07-30 14:55:06 -0400

    Initial Commit

---
+++ WhyDonate/WhyDonateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyDonateApp.swift
+//  WhyDonate
+//
+//  Created by Jibryll Brinkley on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WhyDonateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
